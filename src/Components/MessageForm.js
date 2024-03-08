4f9d7a3a8437d2d2c3a265a6d361706e2fd5a85b
Bhanushi Goyal <bgoyal@ebizneeds.local>
2024-03-08 17:59:44 +0530

    New message Files added

--- src/Components/MessageForm.js
+++ src/Components/MessageForm.js
@@ -1,9 +1,61 @@
-import React from "react"
+import React, { useState } from "react"
+import {SendOutlined, PictureOutlined} from "@ant-design/icons"
+import {sendMessage, isTyping} from "react-chat-engine"
 
-function MessageForm(){
+function MessageForm(props){
 
-    return(
-        <div>This is from message form</div>
+    const [value, setValue] = useState("")
+    const {chatId, creds} = props
+
+    const handleChange = (event) => {
+        setValue(event.target.value)
+    }
+
+    isTyping(props, chatId)
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+        const text = value.trim()
+
+        if (text.length > 0) {
+            sendMessage(creds, chatId, { text })
+        }
+
+        setValue("")
+    }
+
+    /**
+        * Handle upload function
+        * -    it sends the message from the component of creds and chatId
+        *       -   something
+        * -    it is used as a onClick function.
+        * @param {*} event 
+        * @returns {}
+    */
+
+    const handleUpload = (event) => {
+        sendMessage(creds, chatId, { files: event.target.files, text: '' })
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                placeholder="Send a message..."
+                value={value}
+                onChange={handleChange}
+                onSubmit={handleSubmit}
+            />
+            <label htmlFor="upload-button">
+                <span><PictureOutlined /></span>
+            </label>
+            <input
+                type="file"
+                multiple={false}
+                onChange={handleUpload.bind(this)}
+            />
+            <button type="submit"><SendOutlined /></button>
+        </form>
     )
 }
 
